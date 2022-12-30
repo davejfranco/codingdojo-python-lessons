373a16a9e9756c2eca739ae38d861d62e506c6fa
Dave <davefranco1987@gmail.com>
2022-12-31 00:35:27 +0100

    first python lessons

---
+++ func.py
@@ -0,0 +1,28 @@
+
+def imprima_nombre():
+    name = input("Cual es tu nombre? ")
+    print(f"Mi nombre es {name}")
+
+
+def greater(a, b):
+    if a > b:
+        return a#f"Argumento a: {a} es mayor que {b}"   
+    if b > a:
+        return b #f"Argumento b: {b} es mayor que {a}"
+
+#print(greater(4, 6))
+#print(greater(10, 7))
+#print(greater(8, 2))
+
+def print_age():
+    age = int(input("Cual es tu edad? "))
+    for n in range(1, age):
+        print(n)
+
+print_age()
+
+
+
+
+
+
---
+++ loop.py
@@ -0,0 +1,33 @@
+nombres = ["dave", "javiera", "paz", "patrick", "pablo"]
+numeros = [1,2,3,4,5,6]
+
+#For loop
+for name in nombres:
+    if name == "patrick":
+        continue
+    print(name)
+
+
+
+#for n in range(0, len(nombres)):
+#    print(nombres[n])
+
+#for name in nombres:
+#    print(name)
+
+#While loop
+#n = 0
+while len(nombres) > n:
+    print(nombres[n])
+    n = n + 1
+
+age = int(input("Cual es tu edad? "))
+
+print(type(age))
+
+
+
+
+
+
+
